94860aebabdaac134200caf0ec37850d5787600a
charcharus <achrifiansyahfarrel@gmail.com>
2022-05-18 08:59:52 +0700

    first

---
+++ resources/views/UI/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="row mt-4">
+    <div class="col-lg-12 mb-lg-0 mb-4">
+      <div class="card">
+        <div class="card-body p-3">
+          <div class="row">
+            <div class="col-lg-6">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+@endsection
